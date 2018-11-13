cf5dab4c6fd998177dff1cc8dd6de8229d5ab195
xiaonanchong96 <xiaonan.chong14@student.xjtlu.edu.cn>
2018-11-13 16:19:04 +0800

    misunderstanding of variance

--- plot_d.py
+++ plot_d.py
@@ -1,96 +1,138 @@
-import matplotlib
+import matplotlib 
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-
 import answers as an
 
-Xd = np.linspace(0,0.9,25)
-Yd = np.cos(10*Xd**2) + 0.1*np.sin(100*Xd)
-
-def phi(N, M):
-  X = np.linspace(0,0.9,N)
+def Phi(N, M, X):
   phi = np.empty([N, M])
   for i in range(N):
     for j in range(M):
       if(j==0):
         phi[i][j]=1
-      elif(j%2 ==1):
-        J = (j+1)/2
-        phi[i][j]=np.sin(2*np.pi*J*X[i])
       else:
-        J = j/2
-        phi[i][j]=np.cos(2*np.pi*J*X[i])
+        phi[i][j]=np.exp(-(X[i][0]-mu[j-1])**2 / (2*L**2) )
   return phi
 
-phi = phi(25,10)# order!
 
-delta = 0.005#0.005[1-4]
-a = np.arange(0.1, 1.0, delta) #M
-b = np.arange(0.1, 1.0, delta) #N
-m = a.shape[0]
-n = b.shape[0]
-z = np.empty([n,m])
-for a1 in range(m):
-  for b1 in range(n):
-    z[b1][a1] = an.lml(a[a1], b[b1], phi, Yd)
-#print(z.shape)
+def mean_sd(predict):
+  l = predict.shape[0]
+  s = 0
+  for i in range(l):
+    s = s+predict[i]
+  mean = s/float(l)
+
+  ss = 0
+  for i in range(l):
+    dif = (mean-predict[i])**2
+    ss = ss + dif
+  sd = (ss/float(l))**(0.5)
+  return mean, sd
+
+
+alpha = 1
+beta = 0.1 
+N = 25
+M = 10
+
+# TRAINING DATA
+X = np.reshape(np.linspace(0,0.9,N),(N,1))
+y = np.reshape(np.cos(10*X**2) + 0.1*np.sin(100*X),(N,1))
+
+# MODEL order: M
+mu = np.linspace(-0.5, 1, 9)#10 Gausian basis functions equally spaced between -0.5 and 1 
+L = 0.1 #with scale 0.1
+phi = Phi(N, M, X)
+phi_t = np.transpose(phi)
+
+# TESTING DATA
+num = 300
+t = np.linspace(-1, 1.5, num)#for drawing
+Xt = np.reshape(np.linspace(-1, 1.5, num),(num, 1))
+yt = np.reshape(np.cos(10*Xt**2) + 0.1*np.sin(100*Xt),(num,1))
+F = Phi(num, M, Xt)#[300, 10]
+
+nums = 45
+t_sparse = np.linspace(-1, 1.5, nums)#for drawing
+yt_sparse = np.reshape(np.cos(10*t_sparse**2) + 0.1*np.sin(100*t_sparse),(nums,1))
+
+
+
+s0 = alpha * np.identity(M)
+m0 = np.zeros((M, 1))
+
+sn = np.linalg.inv((1/beta)*np.dot(phi_t, phi) + np.linalg.inv(s0))#[M,M]
+mn = np.dot( sn, ((1/beta)*np.dot(phi_t, y) + np.dot(np.linalg.inv(s0), m0)))#[M,1]
+
+## SAMPLE FROM POSTERIOR
+mean = np.ndarray.flatten(mn)
+cov = sn
+size = 5
+thetas = np.random.multivariate_normal(mean, cov, size)
+
+## PREDICTION [num, 5]
+pos_predict = np.zeros((num, 5))
+for i in range(5):
+  theta = thetas[i]
+  pos_predict[:, i] = np.dot(F, theta)
+#print(pos_predict[1].shape[0])#5
+
+pos_pre_sta = np.zeros((num, 2))
+for i in range(num):
+  predict = pos_predict[i]
+  mean, sd = mean_sd(predict)
+  pos_pre_sta[i][0] = mean
+  pos_pre_sta[i][1] = sd
+#print(pos_pre_sta)
+
+mean_curve = np.dot(F, mn)#pos_pre_sta[:,0] #[300,1]
+'''
+u = mean_curve
+b = mean_curve
+V = np.dot(np.dot(F, sn), np.transpose(F))
+for i in range(num):
+  u[i][0] = u[i][0] + V[i][i]
+  upper_curve = np.ndarray.flatten(u)
+  b[i][0] = b[i][0] - V[i][i]
+  bottom_curve = np.ndarray.flatten(b)
+'''
+V = np.zeros(num)
+u = mean_curve
+b = mean_curve
+for i in range(num):
+  phi = F[i]
+  print(np.dot(np.dot(phi, sn), np.transpose(phi)))
+  u[i][0] = mean_curve[i][0] + np.dot(np.dot(phi, sn), np.transpose(phi))*100  
+  b[i][0] = mean_curve[i][0] - np.dot(np.dot(phi, sn), np.transpose(phi))*100
+upper_curve = np.ndarray.flatten(u)
+bottom_curve = np.ndarray.flatten(b)
+
+#upper_curve = pos_pre_sta[:,0] + pos_pre_sta[:,1]
+#bottom_curve = pos_pre_sta[:,0] - pos_pre_sta[:,1]
+
+#print(upper_curve)
+#print(bottom_curve)
+print(upper_curve == bottom_curve)
 
 fig, ax = plt.subplots()
-CS = ax.contour(a,b,z,50)
-ax.clabel(CS, inline=1, fontsize=10)
-ax.set_title('Maximize Log marginal Likelihood - Linear Function Basis')
-#(starting point-(0.9,0.9) step_size = 0.025)
-ax.set_xlabel('alpha >= 0')
-ax.set_ylabel('beta >= 0')
+ax.set_ylim([-2.5,2.5])
+plt.fill_between(t, upper_curve, bottom_curve, color = 'grey', alpha = 0.3)
+
+plt.plot(t_sparse, yt_sparse, 'k+')
+plt.plot(t, mean_curve, lw=2.5, color='k')
+plt.plot(t, upper_curve+beta,'k--')
+plt.plot(t, bottom_curve-beta,'k--')
+for i in range(5):
+  plt.plot(t, pos_predict[:, i], lw=1)
 
 
-def plot2Dpoint(x,y):
-  ax.hold(True)
-  plt.scatter(x, y)
+#'sample 1','sample 2','sample 3','sample 4','sample 5',
+plt.legend(('true data', 'predictive mean', 'noise prediction'),
+           loc='upper right', shadow=True)
 
-def grad(x,y):
-  gama = 0.00005
-  v = True
-  count = 0
-  mlml = 0
-  while(v==True):
-  #for i in range(70):
-    s1=x
-    s2=y
-    #print('s1 :',s1)
-    #print('s2 :',s2)
-    g1=an.grad_lml(x, y, phi, Yd)[0]
-    g2=an.grad_lml(x, y, phi, Yd)[1]
-    x=x+(gama)*g1
-    y=y+(gama)*g2 
-    t1=(gama)*g1
-    t2=(gama)*g2 
-    #print('t1 :',t1)
-    #print('t2 :',t2)
-    ax.arrow(s1,s2,t1,t2, head_width=0.009, head_length=0.01, fc='k', ec='k')
-    #if(i==49):
-      #plot2Dpoint(x,y)
-
-    mlml1 = an.lml(x, y, phi, Yd)
-    
-    if(mlml1-mlml == 0 or count > 10000):
-      if(mlml1-mlml == 0 ):
-        print(mlml1, x, y)
-      v = False
-
-    mlml = mlml1
-    count = count + 1
-
-  #return mlml
-
-
-#plot2Dpoint(0.2,0.2)
-grad(0.2,0.2)
+ax.set_title('Bayesian Linear Regression')
+ax.set_xlabel('input data x')
+ax.set_ylabel('predicted value')
 plt.show()
 
-# order = [0,4]
-# [-28.4944341114782, -27.8019017751, -27.5613740577, -18.2780086987, -19.435735408, 
-# order = [6, ]
-# -9.075980021929912, 
-
+ 
